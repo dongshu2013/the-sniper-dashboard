e9bc374711567b539878abbb97cd8397d15ab4c7
kevintao <taokevin1024@gmail.com>
2025-02-07 16:39:40 +0800

    change name to Curifi

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar() {
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center space-x-2">
             <Logo />
-            <span className="font-bold">Curify</span>
+            <span className="font-bold">Curifi</span>
           </Link>
 
           {user && (
