9da305bfa161b39d8806e9973c2ca42f61de5768
kevintao <taokevin1024@gmail.com>
2025-02-08 16:34:25 +0800

    update bg

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
 }) {
   return (
     <TooltipProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex min-h-screen w-full flex-col bg-muted/60">
         <div className="flex flex-col gap-2 p-2 sm:gap-4 sm:p-4">
           <main className="grid flex-1 items-start gap-2 sm:gap-4">
             {children}
