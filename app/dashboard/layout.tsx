3d17678ec5cbf23e7fa5caf3bfa6d5e37791b1dd
kevintao <taokevin1024@gmail.com>
2025-02-08 18:58:37 +0800

    fix mobile grid

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,9 +12,7 @@ export default function DashboardLayout({
     <TooltipProvider>
       <div className="flex min-h-screen w-full flex-col bg-muted/60">
         <div className="flex flex-col gap-2 p-2 sm:gap-4 sm:p-4">
-          <main className="grid flex-1 items-start gap-2 sm:gap-4">
-            {children}
-          </main>
+          <main className="flex-1">{children}</main>
         </div>
       </div>
     </TooltipProvider>
