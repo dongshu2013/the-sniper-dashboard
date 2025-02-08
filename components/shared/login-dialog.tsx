cc06c37d4ad5fed597112d01d605a9d9320dc54e
xiangnuans <cocoguo7636@gmail.com>
2025-02-08 09:55:50 +0800

    feat: email opt login dialog

--- components/shared/login-dialog.tsx
+++ components/shared/login-dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import {
@@ -15,9 +15,10 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import TelegramLoginButton from '@/components/ui/TelegramLoginButton';
 import { emailLogin } from '@/lib/actions/user';
-import { saveJwt } from '@/components/lib/networkUtils';
+import { getJwt, saveJwt } from '@/components/lib/networkUtils';
 import toast from 'react-hot-toast';
 import { useUserStore } from 'stores/userStore';
+import { Spinner } from 'theme-ui';
 
 export function LoginDialog() {
   const setUser = useUserStore((state) => state.setUser);
@@ -25,42 +26,120 @@ export function LoginDialog() {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [open, setOpen] = useState(false);
+  const [code, setCode] = useState('');
+  const [account, seAccount] = useState('');
+  const [countdown, setCountdown] = useState(0);
+  const [isSending, setIsSending] = useState(false);
+  const token = getJwt();
 
-  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setError(null);
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+    if (countdown > 0) {
+      timer = setTimeout(() => setCountdown(countdown - 1), 1000);
+    }
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, [countdown]);
+
+  const handleSendCode = async () => {
+    if (!account) {
+      return toast.error('Please input valid email and code');
+    }
+    // if (account !== process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
+    //   return toast.error('Only admin can login with email');
+    // }
+    setIsSending(true);
+    const res = await fetch(`/api/auth/email`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: account
+      })
+    });
+
+    console.log('🚀 ~ handleSendCode ~ res', res);
+    const data = await res.json();
+    if (data.code === 0) {
+      setCountdown(60);
+      setCode('');
+      setIsSending(false);
+    } else {
+      setIsSending(false);
+      return toast.error(data?.message || 'Send failed');
+    }
+  };
+
+  const handleLogin = async () => {
+    if (!account || !code) {
+      return toast.error('Please input valid email and code');
+    }
+    // if (account !== process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
+    //   return toast.error('Only admin can login with email');
+    // }
     setIsLoading(true);
-
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-
     try {
-      if (
-        email === process.env.NEXT_PUBLIC_ADMIN_EMAIL &&
-        password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD
-      ) {
-        const res = await emailLogin({
-          email,
-          password
-        });
-        if (res.code === 0) {
-          await saveJwt(res?.data?.token);
-          setUser(res?.data?.user);
-          setOpen(false); // 关闭对话框
-          router.push('/dashboard/overview');
-        } else {
-          toast.error('Login failed!');
-        }
+      const res = await fetch(`/api/auth/verify`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          email: account,
+          code
+        })
+      });
+      const data = await res.json();
+      console.log('🚀 ~ handleLogin ~ data', data);
+      if (data.code === 0) {
+        await saveJwt(data?.data?.token);
+        setUser(data?.data?.user);
+        router.push('/dashboard/overview');
       } else {
-        toast.error(
-          'Invalid email or password(Only admin can login with email).'
-        );
+        return toast.error(data?.message || 'Login failed');
       }
     } finally {
       setIsLoading(false);
     }
-  }
+  };
+  // async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   setError(null);
+  //   setIsLoading(true);
+
+  //   const formData = new FormData(e.currentTarget);
+  //   const email = formData.get('email') as string;
+  //   const password = formData.get('password') as string;
+
+  //   try {
+  //     if (
+  //       email === process.env.NEXT_PUBLIC_ADMIN_EMAIL &&
+  //       password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD
+  //     ) {
+  //       const res = await emailLogin({
+  //         email,
+  //         password
+  //       });
+  //       if (res.code === 0) {
+  //         await saveJwt(res?.data?.token);
+  //         setUser(res?.data?.user);
+  //         setOpen(false); // 关闭对话框
+  //         router.push('/dashboard/overview');
+  //       } else {
+  //         toast.error('Login failed!');
+  //       }
+  //     } else {
+  //       toast.error(
+  //         'Invalid email or password(Only admin can login with email).'
+  //       );
+  //     }
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -76,7 +155,7 @@ export function LoginDialog() {
             Enter your credentials to access the dashboard
           </DialogDescription>
         </DialogHeader>
-        <form onSubmit={onSubmit} className="space-y-4">
+        <div className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
             <Input
@@ -85,17 +164,45 @@ export function LoginDialog() {
               type="email"
               placeholder="admin@gmail.com"
               required
+              onChange={(e) => seAccount(e.target.value)}
             />
           </div>
           <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" required />
+            <Label htmlFor="password">Code</Label>
+            {/* <Input id="password" name="password" type="password" required /> */}
+            <div className="flex flex-row gap-2">
+              <Input
+                placeholder="Verification Code"
+                value={code}
+                onChange={(e: any) => setCode(e.target.value)}
+              />
+              <Button
+                disabled={countdown > 0}
+                className={`
+                  ${countdown > 0 ? 'gray text-gray-600' : 'primary text-white'} 
+                  flex-shrink-0 rounded-[7px]
+                   flex justify-center items-center
+                   ${!account ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                onClick={handleSendCode}
+              >
+                {countdown > 0 ? (
+                  `${countdown}s`
+                ) : isSending ? (
+                  <Spinner
+                    size={20}
+                    sx={{ color: 'white', textAlign: 'center', mr: 3 }}
+                  />
+                ) : (
+                  'Send'
+                )}
+              </Button>
+            </div>
           </div>
           {error && <p className="text-sm text-red-500">{error}</p>}
-          <Button type="submit" className="w-full" disabled={isLoading}>
+          <Button onClick={handleLogin} className="w-full" disabled={isLoading}>
             {isLoading ? 'Signing in...' : 'Sign in'}
           </Button>
-        </form>
+        </div>
         <div className="mt-8 flex items-center justify-center w-full">
           <div className="border-t border-[#ABAFB3] flex-grow" />
           <div className="text-[#202020] text-[14px] text-center font-semibold mx-2 text-nowrap">
