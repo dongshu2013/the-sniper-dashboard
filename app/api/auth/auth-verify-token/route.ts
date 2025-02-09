3f18b84f527f75421906e40e46a0c0c8606a344c
xiangnuans <cocoguo7636@gmail.com>
2025-02-09 18:24:48 +0800

    feat: delete tg logout

--- app/api/auth/auth-verify-token/route.ts
+++ app/api/auth/auth-verify-token/route.ts
@@ -1,38 +1,40 @@
-import { NextResponse } from "next/server";
-import { getJWT, verifyJWT } from "../lib/jwt";
+import { NextResponse } from 'next/server';
+import { getJWT, verifyJWT } from '@/lib/jwt';
 // import { UserPointModel } from "@/models";
 // import connectMongo from "../lib/mongoose"
 
 export async function POST(request: Request) {
   const token =
-    request.headers.get("Authorization")?.replace("Bearer ", "") || "";
+    request.headers.get('Authorization')?.replace('Bearer ', '') || '';
   const jwtSub = await verifyJWT(token);
   const userKey = jwtSub?.userKey;
   const userId = jwtSub?.userId;
   const userKeyType = jwtSub?.userKeyType;
+  const isAdmin = jwtSub?.isAdmin || false;
 
   if (!userKey || !userId || !userKeyType) {
     return NextResponse.json(
-      { code: 401, message: "Auth failed" },
+      { code: 401, message: 'Auth failed' },
       { status: 401 }
     );
   }
 
   try {
     const newToken = await getJWT({
+      isAdmin,
       userId,
       userKey,
-      userKeyType,
+      userKeyType
     });
 
     return NextResponse.json({
       code: 0,
-      data: { token: newToken, userId, userKey, userKeyType },
+      data: { token: newToken, userId, userKey, userKeyType }
     });
   } catch (error) {}
 
   return NextResponse.json(
-    { code: 401, message: "Auth failed" },
+    { code: 401, message: 'Auth failed' },
     { status: 401 }
   );
 }
